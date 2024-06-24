00e8321f8e7df971fc6e82c906705b924e9ccfde
Siripoom Limsoontorn <110332113+poommer@users.noreply.github.com>
2024-06-24 16:07:36 +0700

    compo conversation

---
+++ front-end/src/lib/Learns/conversation.svelte
@@ -0,0 +1,27 @@
+<script>
+   export let data ;
+</script>
+
+<body>
+    {#each data.res as topic}
+        <div>
+            <div
+                class={` ${topic.speaker === "a" ? "" : "flex justify-end items-end"}`}
+            >
+                <div class={`w-[20rem] h-[10rem] flex  ${topic.speaker === "a" ? "" : "flex-row-reverse"}`}>
+                    <div class="flex items-end justify-end">
+                        <img
+                            src={topic.speaker === "a" ? data.speaker_img.a : data.speaker_img.b}
+                            alt=""
+                            class="w-[5rem] rounded-full border-2"
+                        />
+                    </div>
+                    <div class={`w-[20rem] h-[5rem] relative bg-[#373C4A] rounded-[5px] flex flex-col items-center justify-center before:contents-[''] before:absolute before:border-solid before:border-x-[1.5rem] before:border-x-red-600 before:border-x-transparent before:border-t-transparent before:border-y-[1.5rem] before:border-[#373C4A] before:rounded-[10px] ${topic.speaker === "a" ? "before:bottom-0 before:left-[-20px]" : "before:bottom-0 before:right-[-20px]"}`}>
+                        <span class=" text-[#FFC700]">{topic.text_en}</span>
+                        {topic.text_th}
+                    </div>
+                </div>
+            </div>
+        </div>
+    {/each}
+</body>
--- front-end/src/routes/+page.svelte
+++ front-end/src/routes/+page.svelte
@@ -1,11 +1,84 @@
 <script>
-    let chapter1 = {
-        vocab:[],
-        sentence:[],
-
-    }
+    import Conversation from "../lib/Learns/conversation.svelte";
+    let chapter1 = [
+        {
+            type: "vocab",
+            data: [
+                { text: "hello", img: "", status: false },
+                { text: "goodbye", img: "", status: false },
+                { text: "thank you", img: "", status: false },
+                { text: "I", img: "", status: false },
+                { text: "You", img: "", status: false },
+                { text: "boy", img: "", status: false },
+                { text: "girl", img: "", status: false },
+                { text: "apple", img: "", status: false },
+                { text: "eat", img: "", status: false },
+            ],
+        },
+        {
+            type: "sentence",
+            data: [
+                { text: "Hello pussy.", img: "", status: false },
+                { text: "How are you?", img: "", status: false },
+                { text: "very well, thank you.", img: "", status: false },
+                { text: "a boy eats an apple", img: "", status: false },
+                { text: "she is a girl.", img: "", status: false },
+                { text: "I'm a boy and she is a girl", img: "", status: false },
+                { text: "she is a girl.", img: "", status: false },
+            ],
+        },
+        {
+            type: "conversation",
+            speaker_img: {
+                a: "https://cdn-icons-png.flaticon.com/256/9559/9559035.png",
+                b: "https://cdn-icons-png.flaticon.com/256/9725/9725273.png",
+            },
+            res: [
+                {
+                    text_en: "Hello, Jack.",
+                    text_th: "สวัสดีแจ็ค",
+                    speaker: "a",
+                    status: false,
+                },
+                {
+                    text_en: "Hello, Harry",
+                    text_th: "สวัสดีแฮรี",
+                    speaker: "b",
+                    status: false,
+                },
+                {
+                    text_en: "How are you?",
+                    text_th: "คุณเป็นอย่างบ้าง",
+                    speaker: "a",
+                    status: false,
+                },
+                {
+                    text_en: "very well, thank you. and you?",
+                    text_th: "สบายดี แล้วคุณล่ะ",
+                    speaker: "b",
+                    status: false,
+                },
+                {
+                    text_en: "I'm good, thank you.",
+                    text_th: "ฉันสบาย ขอบคุณ",
+                    speaker: "a",
+                    status: false,
+                },
+                {
+                    text_en: "I have to go now. goodbye!",
+                    text_th: "ฉันต้องไปแล้ว ลาก่อน",
+                    speaker: "b",
+                    status: false,
+                },
+                {
+                    text_en: "goodbye!",
+                    text_th: "ลาก่อน",
+                    speaker: "a",
+                    status: false,
+                },
+            ],
+        },
+    ];
 </script>
 
-<body>
-    <h1 class=" text-5xl">good morning!</h1>
-</body>
+<Conversation data={chapter1[2]} />
