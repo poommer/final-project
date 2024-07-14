c946c21a1bb8f8ecdc1e4aac4f06157e4248eded
Siripoom Limsoontorn <110332113+poommer@users.noreply.github.com>
2024-07-14 22:58:40 +0700

    update color tailwind config

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -6,7 +6,9 @@ export default {
       colors: {
         'ec-yello': '#FFC700',
         'ec-light-yello': '#F7CB4E',
-        'ec-dark-bule': '#282C36',
+        'ec-dark-bule-1': '#282C36',
+        'ec-dark-bule-2': '#373C4A',
+        'ec-dark-bule-3': '#484E5F',
         'ec-purple': '#7228BB',
         'ec-green': '#00A725',
         'ec-gray': '#878686',
