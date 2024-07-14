f60b8b1fced2d2d6466f08a620f57f2d46d38253
Siripoom Limsoontorn <110332113+poommer@users.noreply.github.com>
2024-07-14 17:03:59 +0700

    update Bug fix

--- front-end/src/routes/api/+page.server.js
+++ front-end/src/routes/api/+page.server.js
@@ -1,28 +1,21 @@
 // import { BASE_API_URL } from '$env/dynamic/private'
 export async function load({ url }) {
-    let params = new URLSearchParams(url.search);
-    let status = JSON.parse(params.get('status'));
-
-    if(status === 200){
+    try {
+        let params = new URLSearchParams(url.search);
+        let status = JSON.parse(params.get('status'));
         let response = JSON.parse(params.get('response'));
         let { user_ID, user_name, user_email, user_status } = response;
-
-    console.log('Received user data:', { user_ID, user_name, user_email, user_status });
-
-    return {
-
-            status:status,
-            user: { user_ID, user_name, user_email, user_status }
-    };
-    }
-
-    else{
         return {
 
-            status:status
-        }
+            status: status,
+            user: { user_ID, user_name, user_email, user_status }
+        };
+
+
+
+    } catch (err) {
+        return { status: err.message }
     }
-    
 
     ;
 }
--- front-end/src/routes/api/+page.svelte
+++ front-end/src/routes/api/+page.svelte
@@ -1,22 +1,22 @@
 <script>
-	import { onMount } from 'svelte';
+    import { onMount } from 'svelte';
     import { goto } from '$app/navigation';
-    export let data ;
+    export let data;
 
-    
-
-
-onMount(async () => {
-    localStorage.setItem('user', JSON.stringify(data.user))
-    if (data.status === 200 && data.user.user_status === 'verified') {
-        goto('/');
-    }else{
-        goto('register/')
-    }
-});
-
-    
-    
+    onMount(async () => {
+            localStorage.setItem('user', JSON.stringify(data.user));
+            if (data.status === 200 && data.user.user_status === 'verified') {
+                goto('/');
+            } else {
+                goto('register/');
+            }
+    });
 </script>
+
+<!-- {#if data && data.user}
+    {data.user.user_status}
+{:else}
+    <p>User data is not available.</p>
+{/if} -->
 
 {data.user.user_status}
