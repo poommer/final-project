aa0099d2d74ea6d2455ddbe51f61796752c324a3
Siripoom Limsoontorn <110332113+poommer@users.noreply.github.com>
2024-07-14 18:26:07 +0700

    add color therm

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ec-yello': '#FFC700',
+        'ec-light-yello': '#F7CB4E',
+        'ec-dark-bule': '#282C36',
+        'ec-purple': '#7228BB',
+        'ec-green': '#00A725',
+        'ec-gray': '#878686',
+      },
+    },
   },
   plugins: [],
 }
