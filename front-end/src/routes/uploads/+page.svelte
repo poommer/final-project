fc5647d18f70cefbe0d37019a017594c42995a1c
Siripoom Limsoontorn <65209010011@thonburi.ac.th>
2024-08-04 18:49:13 +0700

    upload from

---
+++ front-end/src/routes/uploads/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+  let file;
+
+  async function upload() {
+      const formData = new FormData();
+      formData.append('file', file);
+
+      const response = await fetch('/uploads', {
+          method: 'POST',
+          body: formData
+      });
+
+      if (response.ok) {
+          alert('File uploaded successfully');
+      } else {
+          alert('Failed to upload file');
+      }
+  }
+</script>
+
+<form action="?/upload" method="POST" enctype="multipart/form-data">
+  <input type="password" name="a" id="">
+  <input type="file" name="fileUpload" bind:this={file} />
+  <button>Upload</button>
+</form>
