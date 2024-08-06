02878ad0dbd4f65fa8c4ed92ac1c9a967c77d7ec
pasayakorn <pasayakornj@gmail.com>
2024-08-06 20:20:19 +0700

    hello

--- front-end/src/lib/component/chapterMenu.svelte
+++ front-end/src/lib/component/chapterMenu.svelte
@@ -1,12 +1,31 @@
 <script>
-    
+
 </script>
 <a href="lesson/">chapter1 level1</a>
 <div class=" relative">
-    <button class="z-10 w-[200px] h-[200px] bg-ec-light-yellow rounded-full shadow-[0px_15px__0_0_#755805] active:translate-y-[10px] active:shadow-[0px_10px__0_0_#755805] " >
-        <span class=" text-ec-purple text-[100px]">1</span>
+    <p class=" absolute left-[210px] bottom-[-150px] text-[35px]">chapter1</p>
+    <button class="z-1 w-[140px] h-[140px] bg-ec-light-yellow rounded-full shadow-[0px_10px__0_0_#755805] active:translate-y-[10px] active:shadow-[0px_10px__0_0_#755805]  
+        absolute  bottom-[-300px] left-[200px]"  >
+        <span class=" text-ec-purple text-[80px]">1</span>
     </button>
-    <div class="w-[150px] border-b-[5px] border-dashed absolute left-[120px] bottom-[-20px] z-[-1] transform rotate-45"></div>
-    <button class="z-10 w-[200px] h-[200px] bg-ec-yellow rounded-full shadow-[0px_15px__0_0_#755805] active:translate-y-[10px] active:shadow-[0px_10px__0_0_#755805] absolute bottom-[-200px]" >
+    <div class="w-[150px] border-b-[5px] border-ec-purple border-dashed  absolute left-[320px] bottom-[-310px] z-[-1] transform rotate-45"></div>
+    
+    <button class="z-1 w-[100px] h-[100px] bg-ec-light-yellow rounded-full shadow-[0px_10px__0_0_#755805] active:translate-y-[10px] active:shadow-[0px_10px__0_0_#755805] 
+        absolute left-[440px] bottom-[-440px]" >
     </button>
+    <div class="w-[150px] border-b-[5px] border-ec-purple border-dashed  absolute left-[320px] bottom-[-480px] z-[-10] transform rotate-[-45deg]"></div>
+    <button class="z-1 w-[100px] h-[100px] bg-ec-light-yellow rounded-full shadow-[0px_10px__0_0_#755805] active:translate-y-[10px] active:shadow-[0px_10px__0_0_#755805] 
+        absolute left-[250px] bottom-[-590px]" >
+    </button>
+    <div class="w-[150px] border-b-[5px] border-ec-purple border-dashed  absolute left-[320px] bottom-[-640px] z-[-10] transform rotate-[45deg]"></div>
+    <button class="z-1 w-[100px] h-[100px] bg-ec-light-yellow rounded-full shadow-[0px_10px__0_0_#755805] active:translate-y-[10px] active:shadow-[0px_10px__0_0_#755805] 
+        absolute left-[450px] bottom-[-740px]" >
+    </button>
+    <div class="w-[100px] border-b-[5px] border-ec-purple border-dashed  absolute left-[550px] bottom-[-710px] z-[-10] transform rotate-[5deg]"></div>
+    <p class=" absolute left-[655px] bottom-[-630px] text-[35px]">chapter2</p>
+    <button class="z-1 w-[140px] h-[140px] bg-ec-light-yellow rounded-full shadow-[0px_10px__0_0_#755805] active:translate-y-[10px] active:shadow-[0px_10px__0_0_#755805]  
+        absolute  bottom-[-780px] left-[650px]"  >
+        <span class=" text-ec-purple text-[80px]">2</span>
+    </button>
+
 </div>
