98c5f0d151a32a8803b9f0e7ae810825bf90e391
Siripoom Limsoontorn <65209010011@thonburi.ac.th>
2024-08-08 11:37:17 +0700

    change

--- front-end/src/lib/component/chapterMenu.svelte
+++ front-end/src/lib/component/chapterMenu.svelte
@@ -1,27 +1,42 @@
 <script>
 
 </script>
-<a href="lesson/">chapter1 level1</a>
-<div class=" relative">
-    <p class=" absolute left-[210px] bottom-[-150px] text-[35px]">chapter1</p>
-    <button class="z-1 w-[140px] h-[140px] bg-ec-light-yellow rounded-full shadow-[0px_10px__0_0_#755805] active:translate-y-[10px] active:shadow-[0px_10px__0_0_#755805]  
-        absolute  bottom-[-300px] left-[200px]"  >
-        <span class=" text-ec-purple text-[80px]">1</span>
-    </button>
-    <div class="w-[150px] border-b-[5px] border-ec-purple border-dashed  absolute left-[320px] bottom-[-310px] z-[-1] transform rotate-45"></div>
-    
-    <button class="z-1 w-[100px] h-[100px] bg-ec-light-yellow rounded-full shadow-[0px_10px__0_0_#755805] active:translate-y-[10px] active:shadow-[0px_10px__0_0_#755805] 
-        absolute left-[440px] bottom-[-440px]" >
-    </button>
-    <div class="w-[150px] border-b-[5px] border-ec-purple border-dashed  absolute left-[320px] bottom-[-480px] z-[-10] transform rotate-[-45deg]"></div>
-    <button class="z-1 w-[100px] h-[100px] bg-ec-light-yellow rounded-full shadow-[0px_10px__0_0_#755805] active:translate-y-[10px] active:shadow-[0px_10px__0_0_#755805] 
-        absolute left-[250px] bottom-[-590px]" >
-    </button>
-    <div class="w-[150px] border-b-[5px] border-ec-purple border-dashed  absolute left-[320px] bottom-[-640px] z-[-10] transform rotate-[45deg]"></div>
-    <button class="z-1 w-[100px] h-[100px] bg-ec-light-yellow rounded-full shadow-[0px_10px__0_0_#755805] active:translate-y-[10px] active:shadow-[0px_10px__0_0_#755805] 
+<!-- <a href="lesson/">chapter1 level1</a> -->
+<div class="relative">
+
+        <p class="relative left-[15px] text-[35px]">chapter1</p>
+         <button class="z-1 w-[140px] h-[140px] bg-ec-light-yellow rounded-full shadow-[0px_10px_0_0_#755805] active:translate-y-[5px] active:shadow-[0px_5px_0_0_#755805]  
+            absolute"  >
+            <span class=" text-ec-purple text-[80px]">1</span>
+        </button>
+       <div class="w-[150px] border-b-[5px] border-ec-purple border-dashed  absolute left-[100px] bottom-[-190px] z-[-1] transform rotate-45"></div>
+
+       <button class="z-1 flex justify-center items-center w-[100px] h-[100px] bg-ec-light-yellow rounded-full shadow-[0px_10px_0_0_#755805] active:translate-y-[5px] active:shadow-[0px_5px_0_0_#755805] 
+           absolute left-[440px] bottom-[-140px]" >
+           <img src="icon/book.png" alt="" class="w-16">
+       </button>
+       <div class="w-[150px] border-b-[5px] border-ec-purple border-dashed  absolute left-[320px] bottom-[-480px] z-[-10] transform rotate-[-45deg]"></div>
+
+    <div class="relative">
+    </div>
+
+    <div class="relative">
+        <button class="z-1 flex justify-center items-center w-[100px] h-[100px] bg-ec-light-yellow rounded-full shadow-[0px_10px_0_0_#755805] active:translate-y-[5px] active:shadow-[0px_5px_0_0_#755805] 
+            absolute left-[250px] bottom-[-590px]" > 
+            <img src="icon/chat.png" alt="">
+        </button>
+        <div class="w-[150px] border-b-[5px] border-ec-purple border-dashed  absolute left-[320px] bottom-[-640px] z-[-10] transform rotate-[45deg]"></div>
+    </div>
+
+    <div class="relative">
+
+        <button class="z-1  flex justify-center items-center w-[100px] h-[100px] bg-ec-light-yellow rounded-full shadow-[0px_10px_0_0_#755805] active:translate-y-[5px] active:shadow-[0px_5px_0_0_#755805] 
         absolute left-[450px] bottom-[-740px]" >
-    </button>
-    <div class="w-[100px] border-b-[5px] border-ec-purple border-dashed  absolute left-[550px] bottom-[-710px] z-[-10] transform rotate-[5deg]"></div>
+        <img src="icon/quiz.png" alt="">
+        </button>
+        <div class="w-[100px] border-b-[5px] border-ec-purple border-dashed  absolute left-[550px] bottom-[-710px] z-[-10] transform rotate-[deg]"></div>
+    </div>
+    
     <p class=" absolute left-[655px] bottom-[-630px] text-[35px]">chapter2</p>
     <button class="z-1 w-[140px] h-[140px] bg-ec-light-yellow rounded-full shadow-[0px_10px__0_0_#755805] active:translate-y-[10px] active:shadow-[0px_10px__0_0_#755805]  
         absolute  bottom-[-780px] left-[650px]"  >
