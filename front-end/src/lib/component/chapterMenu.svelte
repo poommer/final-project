a0648bb3f6c22b5f36f23f4c690015f768ee7d7b
Siripoom Limsoontorn <65209010011@thonburi.ac.th>
2024-08-19 22:15:55 +0700

    update8/19/2024

--- front-end/src/lib/component/chapterMenu.svelte
+++ front-end/src/lib/component/chapterMenu.svelte
@@ -5,49 +5,49 @@
 
     <div class="relative">
         <p class="left-[15px] text-[35px]">chapter1</p>
-         <button class="z-1 w-[140px] h-[140px] bg-ec-light-yellow rounded-full shadow-[0px_10px_0_0_#755805] active:translate-y-[5px] active:shadow-[0px_5px_0_0_#755805]  
-            "  >
-            <span class=" text-ec-purple text-[80px]">1</span>
+         <button class="z-1 w-[140px] h-[140px] bg-ec-light-yellow text-ec-purple text-[80px] rounded-full shadow-[0px_10px_0_0_#755805] active:translate-y-[5px] active:shadow-[0px_5px_0_0_#755805]  
+            disabled:cursor-no-drop disabled:bg-gray-500 disabled:shadow-[0px_10px_0_0_#9b9b9b] disabled:text-stone-800 disabled:translate-y-0" >
+            1
         </button>
 
         <div class="relative w-[150px] border-b-[5px] border-ec-purple border-dashed ml-[100px] z-[-1] transform rotate-45"></div>
         <button class="z-1 flex justify-center items-center w-[100px] h-[100px] bg-ec-light-yellow rounded-full shadow-[0px_10px_0_0_#755805] active:translate-y-[5px] active:shadow-[0px_5px_0_0_#755805] 
-             ml-[210px] bottom-[-330px]" >
+             ml-[210px] bottom-[-330px] disabled:cursor-no-drop disabled:bg-gray-500 disabled:shadow-[0px_10px_0_0_#9b9b9b] disabled:text-stone-800 disabled:active:translate-y-0" >
             <img src="icon/book.png" alt="" class="w-16">
         </button>
         
         <div class="relative w-[150px] border-b-[5px] border-ec-purple border-dashed ml-[100px] z-[-1] transform rotate-[-45deg]"></div>
         <button class="z-1 flex justify-center items-center w-[100px] h-[100px] bg-ec-light-yellow rounded-full shadow-[0px_10px_0_0_#755805] active:translate-y-[5px] active:shadow-[0px_5px_0_0_#755805] 
-             ml-[50px] bottom-[-510px]" > 
+             ml-[50px] bottom-[-510px]disabled:cursor-no-drop disabled:bg-gray-500 disabled:shadow-[0px_10px_0_0_#9b9b9b] disabled:text-stone-800 disabled:active:translate-y-0"  > 
             <img src="icon/chat.png" alt="">
         </button>
-        <div class="relative w-[150px] border-b-[5px] border-ec-purple border-dashed ml-[100px] z-[-1] transform rotate-45"></div>
+        <div class="relative w-[150px] border-b-[5px] border-gray-500 border-dashed ml-[100px] z-[-1] transform rotate-45"></div>
         <button class="z-1 flex justify-center items-center w-[100px] h-[100px] bg-ec-light-yellow rounded-full shadow-[0px_10px_0_0_#755805] active:translate-y-[5px] active:shadow-[0px_5px_0_0_#755805] 
-             ml-[210px] bottom-[-330px]" >
+             ml-[210px] bottom-[-330px] disabled:cursor-no-drop disabled:bg-gray-500 disabled:shadow-[0px_10px_0_0_#9b9b9b] disabled:text-stone-800 disabled:active:translate-y-0" disabled >
             <img src="icon/book.png" alt="" class="w-16">
         </button>
 
-        <div class="relative w-[150px] border-b-[5px] border-ec-purple border-dashed ml-[100px]  z-[-1] transform rotate-[-45deg]"></div>
+        <div class="relative w-[150px] border-b-[5px] border-gray-500    border-dashed ml-[100px]  z-[-1] transform rotate-[-45deg]"></div>
         <button class="z-1 flex justify-center items-center w-[100px] h-[100px] bg-ec-light-yellow rounded-full shadow-[0px_10px_0_0_#755805] active:translate-y-[5px] active:shadow-[0px_5px_0_0_#755805] 
-             ml-[50px] bottom-[-510px]" > 
+             ml-[50px] bottom-[-510px] disabled:cursor-no-drop disabled:bg-gray-500 disabled:shadow-[0px_10px_0_0_#9b9b9b] disabled:text-stone-800 disabled:active:translate-y-0" disabled > 
             <img src="icon/chat.png" alt="">
         </button>
-        <div class="relative w-[150px] border-b-[5px] border-ec-purple border-dashed ml-[100px] z-[-1] transform rotate-45"></div>
+        <div class="relative w-[150px] border-b-[5px] border-gray-500    border-dashed ml-[100px] z-[-1] transform rotate-45"></div>
         <button class="z-1 flex justify-center items-center w-[100px] h-[100px] bg-ec-light-yellow rounded-full shadow-[0px_10px_0_0_#755805] active:translate-y-[5px] active:shadow-[0px_5px_0_0_#755805] 
-        ml-[210px] bottom-[-330px]" >
+        ml-[210px] bottom-[-330px] disabled:cursor-no-drop disabled:bg-gray-500 disabled:shadow-[0px_10px_0_0_#9b9b9b] disabled:text-stone-800 disabled:active:translate-y-0" disabled >
         <img src="icon/book.png" alt="" class="w-16">
     </button>
-    <div class="relative w-[150px] border-b-[5px] border-ec-purple border-dashed ml-[100px] z-[-1] transform rotate-[-45deg]"></div>
+    <div class="relative w-[150px] border-b-[5px] border-gray-500    border-dashed ml-[100px] z-[-1] transform rotate-[-45deg]"></div>
     <button class="z-1 flex justify-center items-center w-[100px] h-[100px] bg-ec-light-yellow rounded-full shadow-[0px_10px_0_0_#755805] active:translate-y-[5px] active:shadow-[0px_5px_0_0_#755805] 
-    ml-[50px] bottom-[-510px]" > 
+    ml-[50px] bottom-[-510px] disabled:cursor-no-drop disabled:bg-gray-500 disabled:shadow-[0px_10px_0_0_#9b9b9b] disabled:text-stone-800 disabled:active:translate-y-0" disabled > 
     <img src="icon/chat.png" alt="">
 </button>
 
-<div class="relative w-[150px] border-b-[5px] border-ec-purple border-dashed ml-[100px] z-[-1] transform rotate-45"></div>
+<div class="relative w-[150px] border-b-[5px] border-gray-500    border-dashed ml-[100px] z-[-1] transform rotate-45"></div>
         
                 
         <button class="z-1  flex justify-center items-center w-[100px] h-[100px] bg-ec-light-yellow rounded-full shadow-[0px_10px_0_0_#755805] active:translate-y-[5px] active:shadow-[0px_5px_0_0_#755805] 
-         ml-[220px] bottom-[-1400px]" >
+         ml-[220px] bottom-[-1400px] disabled:cursor-no-drop disabled:bg-gray-500 disabled:shadow-[0px_10px_0_0_#9b9b9b] disabled:text-stone-800 disabled:active:translate-y-0" disabled >
         <img src="icon/quiz.png" alt="">
         </button>
     </div>
