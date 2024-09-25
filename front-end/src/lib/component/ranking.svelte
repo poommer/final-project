8be42502686a8fa43bbe97e820b9761a97d35384
pasayakorn <pasayakornj@gmail.com>
2024-09-25 22:20:03 +0700

    ranking

--- front-end/src/lib/component/ranking.svelte
+++ front-end/src/lib/component/ranking.svelte
@@ -1,38 +1,117 @@
 <script>
-	 import axios from 'axios'
-  import { onMount } from 'svelte';
-
-  let userID
-      let loadData = async() => {
-        loadData = await axios.get('http://localhost:3000/api/user/rank')
-        console.log(loadData);
-        userID = await JSON.parse(localStorage.getItem('user')).user_ID
-        console.log(userID);
-        return loadData
-        
-      }
+	 
 </script>
 
 <div class=" bg-ec-dark-blue-4 w-11/12 mt-4 rounded-lg p-4">
     <h1 class="text-5xl text-center uppercase text-ec-yellow-400">EC Ranking</h1>
-    {#await loadData()}
-        loading....
-    {:then data} 
-            {#each data.data as item, index }
-            {#if item.user_ID === userID}
-            <div class="myRank bg-ec-purple-500">
-                <span class=" text-ec-yellow">{index+1}</span>
-                {item.user_name}
-                <span class=" text-ec-yellow">exp:{item.xp}</span>
-                your rank
+    <div class=" flex text-center ml-[10px]">
+        <div class=" z-40 w-[100px] h-[100px] rounded-full bg-ec-purple text-center text-[60px] text-ec-yellow">
+            20
+        </div>
+        <div class=" text-ec-yellow">
+            <span class="ml-[-100px]">YOUR NAME [MY RANK]</span>
+            <div class=" flex w-[350px] h-[50px] rounded-3xl bg-ec-light-yellow z-10 gap-[15px] text-[20px]"> 
+                <div class="w-[120px] h-[30px] flex flex-row bg-ec-dark-blue-1 rounded-2xl gap-[10px] mt-[10px] ml-[10px]">
+                    <img src="/icon/xp.png" alt="" class=" w-[25px] h-[25px] ml-[5px]">
+                    <p>3200</p>
+                  </div>
+
+                <div class="w-[120px] h-[30px] flex flex-row bg-ec-dark-blue-1 rounded-2xl gap-[15px] mt-[10px] ">
+                    <img src="/icon/coin.png" alt="" class=" w-[20px] h-[20px] mt-[7px] ml-[10px]">
+                    <p>120</p>
+                  </div>
+                  
             </div>
-            {:else}
-            <div>
-                <span class=" text-ec-yellow">{index+1}</span>
-                {item.user_name}
-                <span class=" text-ec-yellow">exp:{item.xp}</span>
+        </div>
+        
+    </div>
+    <span class=" text-ec-yellow">ALL RANKING</span>
+
+    <div class="">
+        <div class=" flex text-center">
+            <div class="">
+                <img src="/icon/first.png" alt="" width="120px" height="120px">
             </div>
-            {/if}
-            {/each}
-    {/await}
+            <div class=" text-ec-yellow">
+                <span class="ml-[-200px]">YOUR NAME</span>
+                <div class=" flex w-[350px] h-[50px] rounded-3xl bg-ec-dark-blue-5 z-10 gap-[15px] text-[20px]"> 
+                    <div class="w-[120px] h-[30px] flex flex-row bg-ec-dark-blue-1 rounded-2xl gap-[10px] mt-[10px] ml-[10px]">
+                        <img src="/icon/xp.png" alt="" class=" w-[25px] h-[25px] ml-[5px]">
+                        <p>3200</p>
+                      </div>
+    
+                    <div class="w-[120px] h-[30px] flex flex-row bg-ec-dark-blue-1 rounded-2xl gap-[15px] mt-[10px] ">
+                        <img src="/icon/coin.png" alt="" class=" w-[20px] h-[20px] mt-[7px] ml-[10px]">
+                        <p>120</p>
+                      </div>
+                      
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+    <div class="">
+        <div class=" flex text-center">
+            <div class="">
+                <img src="/icon/second.png" alt="" width="120px" height="120px">
+            </div>
+            <div class=" text-ec-yellow">
+                <span class="ml-[-200px]">YOUR NAME</span>
+                <div class=" flex w-[350px] h-[50px] rounded-3xl bg-ec-dark-blue-5 z-10 gap-[15px] text-[20px]"> 
+                    <div class="w-[120px] h-[30px] flex flex-row bg-ec-dark-blue-1 rounded-2xl gap-[10px] mt-[10px] ml-[10px]">
+                        <img src="/icon/xp.png" alt="" class=" w-[25px] h-[25px] ml-[5px]">
+                        <p>3200</p>
+                      </div>
+    
+                    <div class="w-[120px] h-[30px] flex flex-row bg-ec-dark-blue-1 rounded-2xl gap-[15px] mt-[10px] ">
+                        <img src="/icon/coin.png" alt="" class=" w-[20px] h-[20px] mt-[7px] ml-[10px]">
+                        <p>120</p>
+                      </div>
+                      
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+    <div class="">
+        <div class=" flex text-center">
+            <div class="">
+                <img src="/icon/third.png" alt="" width="120px" height="120px">
+            </div>
+            <div class=" text-ec-yellow">
+                <span class="ml-[-200px]">YOUR NAME</span>
+                <div class=" flex w-[350px] h-[50px] rounded-3xl bg-ec-dark-blue-5 z-10 gap-[15px] text-[20px]"> 
+                    <div class="w-[120px] h-[30px] flex flex-row bg-ec-dark-blue-1 rounded-2xl gap-[10px] mt-[10px] ml-[10px]">
+                        <img src="/icon/xp.png" alt="" class=" w-[25px] h-[25px] ml-[5px]">
+                        <p>3200</p>
+                      </div>
+    
+                    <div class="w-[120px] h-[30px] flex flex-row bg-ec-dark-blue-1 rounded-2xl gap-[15px] mt-[10px] ">
+                        <img src="/icon/coin.png" alt="" class=" w-[20px] h-[20px] mt-[7px] ml-[10px]">
+                        <p>120</p>
+                      </div>
+                      
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    
+    <div class="flex flex-row text-[25px] gap-[30px]">
+    <span class="mt-[5px] text-ec-yellow">#4 YOUR NAME</span>
+    <div class=" flex  z-10 gap-[15px] text-[20px]"> 
+        <div class="w-[120px] h-[30px] flex flex-row bg-ec-dark-blue-1 rounded-2xl gap-[10px] mt-[10px] ml-[10px]">
+            <img src="/icon/xp.png" alt="" class=" w-[25px] h-[25px] ml-[5px]">
+            <p>3200</p>
+          </div>
+
+        <div class="w-[120px] h-[30px] flex flex-row bg-ec-dark-blue-1 rounded-2xl gap-[15px] mt-[10px] ">
+            <img src="/icon/coin.png" alt="" class=" w-[20px] h-[20px] mt-[7px] ml-[10px]">
+            <p>120</p>
+          </div>
+          
+    </div>
+    </div>
 </div>
